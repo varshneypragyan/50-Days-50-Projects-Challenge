c4e3724428ec2bc0eee6b714f4c8962e182146f2
Pragyan Varshney <haillordprometheus@gmail.com>
2021-05-08 10:13:30 +0530

    Day-1 project

---
+++ Day-1/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".panel"); // returns a nodelist (similar to an array)
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
